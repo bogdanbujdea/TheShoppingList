dbf3e72774e63f5352202a9341ee0d443aae2e0b
bujdeabogdan <bujdeabogdan@gmail.com>
2014-02-04 02:06:29 +0200

    first store version

---
+++ Settings/PrivacyPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using TheShoppingList.Common;
+using Windows.UI.Xaml.Controls;
+
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
+
+namespace TheShoppingList.Settings
+{
+    /// <summary>
+    /// A basic page that provides characteristics common to most applications.
+    /// </summary>
+    public sealed partial class PrivacyPage
+    {
+        public PrivacyPage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="navigationParameter">The parameter value passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested.
+        /// </param>
+        /// <param name="pageState">A dictionary of state preserved by this page during an earlier
+        /// session.  This will be null the first time a page is visited.</param>
+        protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+        {
+            Assembly assembly = typeof (App).GetTypeInfo().Assembly;
+            Stream htmlStream = assembly.GetManifestResourceStream("TheShoppingList.Settings.privacy.html");
+            var reader = new StreamReader(htmlStream);
+            string readToEnd = reader.ReadToEnd();
+            privacyWebView.NavigateToString(readToEnd);
+            //privacyWebView.Source = new Uri("ms-appx:/privacy.html", UriKind.RelativeOrAbsolute);
+        }
+
+        /// <summary>
+        /// Preserves state associated with this page in case the application is suspended or the
+        /// page is discarded from the navigation cache.  Values must conform to the serialization
+        /// requirements of <see cref="SuspensionManager.SessionState"/>.
+        /// </summary>
+        /// <param name="pageState">An empty dictionary to be populated with serializable state.</param>
+        protected override void SaveState(Dictionary<String, Object> pageState)
+        {
+        }
+    }
+}
